18e1c0c14b1d65dadaef3775737e735a9181477e
kimsangjunv1 <ingyoe64@gmail.com>
2023-07-30 02:07:34 +0900

    Improve: 페이지 생성

--- src/app/list/page.js
+++ src/app/list/page.js
@@ -1,8 +1,7 @@
 export default function Home() {
-  let name = "park";
   return (
     <div>
-      <h2 className="title">{name}</h2>
+      <h2 className="title">상품목록</h2>
       <p className="title-sub">by dev Like</p>
     </div>
   );
