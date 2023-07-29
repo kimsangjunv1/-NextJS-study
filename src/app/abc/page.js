f57b43f60fc19bfe2c57763e0d7c2508bcf31e64
kimsangjunv1 <ingyoe64@gmail.com>
2023-07-30 02:31:38 +0900

    Improve: 페이지 별 nav 설정

---
+++ src/app/abc/page.js
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <div>
+      <p>이곳은 abc 페이지 입니다.</p>
+    </div>
+  );
+}
--- src/app/list/page.js
+++ src/app/list/page.js
@@ -1,8 +1,13 @@
-export default function Home() {
+export default function List() {
   return (
     <div>
       <h2 className="title">상품목록</h2>
-      <p className="title-sub">by dev Like</p>
+      <div className="food">
+        <h2>상품1 1000원</h2>
+      </div>
+      <div className="food">
+        <h2>상품2 200원</h2>
+      </div>
     </div>
   );
 }
